92b1bddccf29aad22c4d85f64c3348a20a18e3cb
Martin Ricciardi <martin.ricciardi.04@gmail.com>
2023-08-07 20:25:26 -0300

    id de transaccion

--- datos.js
+++ datos.js
@@ -7,12 +7,13 @@ let moneda = prompt("Ingrese que moneda quiere comprar").toUpperCase()
 let cantidad = Number(prompt("Ingrese que cantidad quiere comprar"))
 
 class Datos {
-    constructor(nombre, apellidos, dni, moneda, cantidad){
+    constructor(nombre, apellidos, dni, moneda, cantidad, id){
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.dni = dni;
         this.moneda = moneda;
         this.cantidad = cantidad;
+        this.id = id;
         this.validado = false;
     }
     validar(){
@@ -20,6 +21,8 @@ class Datos {
     }
 }
 
-datos.push(new Datos(nombre, apellido, dni, moneda, cantidad))
+let id = Math.round(Math.random() * 1000000)
 
-datos.forEach((item) => {alert(`Datos del comprador: Nombre: ${item.nombre} Apellido: ${item.apellidos} DNI: ${item.dni} Moneda: ${item.moneda} Cantidad: ${item.cantidad} Validado: ${item.validado}`)})
+datos.push(new Datos(nombre, apellido, dni, moneda, cantidad, id))
+
+datos.forEach((item) => {alert(`Datos del comprador: Nombre: ${item.nombre} Apellido: ${item.apellidos} DNI: ${item.dni} Moneda: ${item.moneda} Cantidad: ${item.cantidad} Validado: ${item.validado} ID de transaccion: #${item.id}`)})
